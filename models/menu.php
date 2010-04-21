795619f6f7d95b17db033f7745545f08945b1e63
Henry Stivens Adarme Muñoz <henry@henry-laptop.(none)>
2010-04-22 05:18:09 +0800

    Nuevos modelos

---
+++ models/menu.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * KBlog - KumbiaPHP Blog
+ * PHP version 5
+ * LICENSE
+ *
+ * This source file is subject to the GNU/GPL that is bundled
+ * with this package in the file docs/LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to deivinsontejeda@gmail.com so we can send you a copy immediately.
+ *
+ * @author Deivinson Tejeda <deivinsontejeda@gmail.com>
+ */
+//require_once APP_PATH.'active_record.php';
+class Menu extends ActiveRecord
+{}
---
+++ models/usuario.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * KBlog - KumbiaPHP Blog
+ * PHP version 5
+ * LICENSE
+ *
+ * This source file is subject to the GNU/GPL that is bundled
+ * with this package in the file docs/LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to deivinsontejeda@gmail.com so we can send you a copy immediately.
+ *
+ * @author Deivinson Tejeda <deivinsontejeda@gmail.com>
+ */
+class Usuario extends ActiveRecord {
+    public function initialize() {
+        //relaciones
+        $this->belongs_to('perfil');
+        $this->validates_email_in('mail', array('message'=>'Campo mail incorrecto'));
+    }
+
+    /**
+     * Obtiene una lista de Usuarios
+     * paginada
+     *
+     * @param int $page
+     * @param string $status
+     */
+    public function getListUsuarios($page = 1, $status = 'A') {
+        return $this->paginate("conditions: status= '$status'", "page: $page");
+    }
+
+    /**
+     * Cambia el pass de un usuario
+     *
+     * @param int $idUser
+     * @param string $passOld
+     * @param string $passNew
+     * @return bool
+     */
+    public function changePass($idUser, $passOld, $passNew) {
+        $user = $this->find($idUser);
+        $passOld = sha1($passOld);
+        if($user->password == $passOld) {
+            $user->password = sha1($passNew);
+            if($user->save()) {
+                Flash::success('Cambio de Clave se efectuó');
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * verifica la existencia de un usuario
+     * mediante su mail
+     *
+     * @param string $mail
+     * @return bool
+     */
+    public function loginExist($mail) {
+        return $this->exists("login='$mail'");
+    }
+
+    /**
+     * Obtiene los datos de un Usuario dado su mail
+     *
+     * @param string $login
+     * @param string $pass
+     * @return resulset
+     */
+    public function updateUsuarioByMail($login, $pass) {
+        $this->find_first("login='$login'");
+        $this->password = sha1($pass);
+        $this->update();
+        return $this;
+    }
+}
