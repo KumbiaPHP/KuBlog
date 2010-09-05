d25332b687d356a359a0a51ddfa41ac15d97ef3a
Henry Stivens Adarme Muñoz <henry@henry-laptop.(none)>
2010-09-05 17:46:25 -0500

    Adicionando estos archivos al repositorio, que al parece no se incluyeron al principio.

---
+++ app/controllers/admin/galeria_controller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Description of galeria_controller.php
+ * 10/05/2010 08:45:35 PM
+ *
+ * @author henry
+ * @copyright 2010
+ * @license Put project's license
+ */
+class GaleriaController extends ApplicationController{
+    public function index(){
+
+    }
+}
+?>
